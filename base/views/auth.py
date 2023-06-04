a714a16377e4d4944898c69359497e3ad0f2eab2
alex <funke2961@gmail.com>
2023-06-04 19:40:40 +0600

    update sign up

--- base/views/auth.py
+++ base/views/auth.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import login,logout, authenticate
+from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth.models import User
 
 from ..forms import CreateUserForm
 from ..utils import auth
@@ -19,7 +21,7 @@ def sign_in(request):
          login(request, user)
          return redirect('base:create-chat')
       else:
-         messages.error(request,'Email or password incorrect')
+         messages.error(request, 'Email or password incorrect')
          return redirect('base:sign-in')
    
    context = {
@@ -29,23 +31,42 @@ def sign_in(request):
 
 def sign_up(request):
    type = 'sign_up'
-   form = CreateUserForm()
    
    if request.method == 'POST':
-      user = CreateUserForm(request.POST)
-      if user.is_valid():
-         user = user.save()
+      username = request.POST.get('username')
+      email = request.POST.get('email')
+      password = request.POST.get('password')
+        
+      try:
+         isHaveEmail = User.objects.get(email=email)
          
-         login(request, user)
-         return redirect('base:create-chat')
+         messages.error(request, 'Email is already exist')
+         return redirect('base:sign-in')
+      except:
+         print('Email does not exist')
+         
+      try:
+         isHaveUsername = User.objects.get(username=username)
+         
+         messages.error(request, 'Username is already exist')
+         return redirect('base:sign-in')
+      except:
+         print('Username does not exist')
+      
+      user = User.objects.create(
+         username = username,
+         email = email,
+         password = make_password(password),
+      )
+         
+      login(request, user)
+      return redirect('base:create-chat')
    
    context = {
       'page_type': type,
-      'form': form,
    }
    return render(request, 'base/Auth.html', context)
 
 def sign_out(request):
-   request.logout()
-   
+   logout(request)
    return redirect('base:sign-in')
--- static/js/show_menu.js
+++ static/js/show_menu.js
@@ -2,5 +2,11 @@ const header__left__menu = document.querySelector('.header__left__menu')
 const show__menu = document.querySelector('.show__menu')
 
 show__menu.addEventListener('click', () => {
-   
+   if (header__left__menu.style.display == 'none') {
+      header__left__menu.style.display = 'flex'
+      show__menu.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+   } else {
+      header__left__menu.style.display = 'none'
+      show__menu.innerHTML = '<i class="fa-solid fa-bars"></i>'
+   }
 })
